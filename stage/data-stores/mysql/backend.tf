5990fd26f1881db376f2aecf2be3a33b7a437c4d
Kevin McGarry <kevinjmcgarry@gmail.com>
2019-11-07 11:32:57 -0600

    added backends for stage data-store and web service

--- stage/data-stores/mysql/backend.tf
+++ stage/data-stores/mysql/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket          = "terraform-12-demo-state"
-        key             = "stage/services/webserver-cluster/terraform.tfstate"
+        key             = "stage/services/data-stores/mysql/terraform.tfstate"
         region          = "us-west-2"
         dynamodb_table  = "terraform-12-demo-locks"
         encrypt         = "true"
--- stage/data-stores/mysql/main.tf
+++ stage/data-stores/mysql/main.tf
@@ -9,19 +9,6 @@ provider "aws" {
   version = "~> 2.0"
 }
 
-terraform {
-  backend "s3" {
-    # This backend configuration is filled in automatically at test time by Terratest. If you wish to run this example
-    # manually, uncomment and fill in the config below.
-
-    # bucket         = "<YOUR S3 BUCKET>"
-    # key            = "<SOME PATH>/terraform.tfstate"
-    # region         = "us-east-2"
-    # dynamodb_table = "<YOUR DYNAMODB TABLE>"
-    # encrypt        = true
-  }
-}
-
 resource "aws_db_instance" "example" {
   identifier_prefix   = "terraform-up-and-running"
   engine              = "mysql"
